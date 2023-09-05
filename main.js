0e67cc10fa59e8d592eabdd07c6a105828602b22
edisonmaya <ee_mayahuayamave2@hotmail.com>
2023-09-04 19:12:39 -0500

    cambios en el background2

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ value.textContent = count
 
 function c_color ()
 { 
+    if(count===0)                   return body.style.background='white'
     if(count <= 5)                  return value.style.color='black', value.style.border='1px solid black', h1.style.color='black', body.style.background='black'
     if(count > 5 && count <= 10)    return value.style.color='blue' , value.style.border='2px solid blue' , h1.style.color='blue' , body.style.background='blue'  
     else                            return value.style.color='red'  , value.style.border='3px solid red'  , h1.style.color='red'  , body.style.background='red'
@@ -23,6 +24,11 @@ inc.addEventListener("click", function() {
 dec.addEventListener("click", function() {
     if (count > 0) {
         count--
+        c_color ()
+        value.textContent = count;
+    }
+    if(count===0)
+    {
         c_color ()
         value.textContent = count;
     }
